0736f336854acc1c1e3278300193504a896bfa55
Jose Gonzalez <only1gonzalez@gmail.com>
2018-05-26 18:19:52 -0600

    Testing github commits

--- src/main/java/bean/SquadMaker.java
+++ src/main/java/bean/SquadMaker.java
@@ -2,16 +2,29 @@ package bean;
 
 import java.io.Serializable;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import domain.League;
+
 @ManagedBean
 @SessionScoped
-public class HelloBean  implements Serializable{
+public class SquadMaker implements Serializable {
 
 	private static final long serialVersionUID = -4823295172962937652L;
-	
-	private String message = "Your Java app is up and running on Azure";
+
+	private String message = "Your SquadMaker is up and away!!!";
+	private League league;
+
+	@PostConstruct
+	public void init() {
+		league = new League();
+
+		// Retrieve player list
+
+		// populate waiting list
+	}
 
 	public String getMessage() {
 		return message;
@@ -19,5 +32,5 @@ public class HelloBean  implements Serializable{
 
 	public void setMessage(String message) {
 		this.message = message;
-	}	
+	}
 }
